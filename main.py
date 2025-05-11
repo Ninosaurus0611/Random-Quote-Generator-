1dd0278590fc5d599d1e288142df91242924bc81
Ninosaurus0611 <nino.daniele@ziggo.nl>
2025-05-11 20:22:44 +0200

    poging

---
+++ main.py
@@ -0,0 +1,27 @@
+# Random Quote Generator
+
+# benodigde onderdelen:
+# lijst met Quotes (miss veranderen als mogelijk naar vanuit quote word bestand)
+# functie die een random quote print
+#UI -> site of scherm dat zich opent om de quote te zien (miss met een knop)
+
+#imports??:
+#import json
+#import random
+
+
+#Selectie -> random quote, specifiek genre, of autheur
+#random quote kiezen
+#quote tonen
+
+import random
+
+categories = ["self improvement", "power"]
+
+
+specifieke_categorie = input("Wil je een quote van een bepaalde categorie? (of druk Enter voor willekeurig): ").strip()
+if specifieke_categorie == "ja" or "Ja":
+    print(f"Dit zijn de categoriën", categories)
+else:
+    print()
+
