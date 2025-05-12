33abe642c17f3927a9938a19f90deea5a0b5c20d
Ninosaurus0611 <nino.daniele@ziggo.nl>
2025-05-12 11:01:20 +0200

    poging

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ category = sorted({q['category'] for q in quotes})
 
 random_quote = random.choice(quotes)
 
-specifieke_categorie = input("Wilt u een specifieke categorie? Zo niet, druk Enter")
+specifieke_categorie = input("Wilt u een specifieke categorie? Zo niet, druk Enter\n")
 if specifieke_categorie.strip().lower() == "ja":
     print("Beschikbare categorieën:", category)
 
